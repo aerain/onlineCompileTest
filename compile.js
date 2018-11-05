de19fa35d57a321c5beb1dcc7bb1357c2e025d6c
aerain <j40f893@gmail.com>
2018-11-05 16:44:01 +0900

    system 함수 차단

--- compile.js
+++ compile.js
@@ -9,8 +9,11 @@ var bufferData = '',
 router.post('/', function(req, res, next) {
 
     let { sourceCode } = req.body;
-    console.log(sourceCode + " 입니다.");
-
+    console.log(sourceCode.match(/system/g));
+    if(sourceCode.match(/system/g) !== null) {
+        res.send('{"error": "system 함수는 사용할 수 없습니다."}');
+        return;
+    }
     fs.writeFile('compile/main.c', sourceCode, 'utf8', (err) => {
         if(err) {
             res.send(err);
--- compile/main.c
+++ compile/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(void) {
 printf("승주형 사랑해요\n");
 return 0;
